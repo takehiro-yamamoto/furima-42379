eee147ad477a8d11d9f2f9ca81a11c10ae8ed1ff
takehiro-yamamoto <xj543ade@gmail.com>
2025-05-15 12:39:12 +0900

    ユーザー管理機能のストロングパラメーターを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller? # Deviseのコントローラの場合のみ実行
+  
   private
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
-    end
+  # Strong Parameters
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date
+    ])# ユーザー新規登録の際にparams以外の指定カラムを許可する
+    
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date
+    ])# ユーザー情報更新の際にparams以外の指定カラムを許可する 
   end
 end
