4b5a2ac8112c7cb3458b4376cee086b35ffe9d83
takehiro-yamamoto <xj543ade@gmail.com>
2025-05-16 10:57:40 +0900

    ストロングパラメーターの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [
                                         :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date
                                       ]) # ユーザー新規登録の際にparams以外の指定カラムを許可する
-
-    devise_parameter_sanitizer.permit(:account_update, keys: [
-                                        :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date
-                                      ]) # ユーザー情報更新の際にparams以外の指定カラムを許可する
   end
 end
