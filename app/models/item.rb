2db06634151b86212ac269c5c0f3a62e2523523c
takehiro-yamamoto <xj543ade@gmail.com>
2025-05-16 22:28:49 +0900

    ActiveHash周りの設定修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,16 +5,16 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :shipping_fee_status
+  belongs_to :shipping_fee
   belongs_to :shipping_area
-  belongs_to :shipping_days
+  belongs_to :shipping_day
 
   with_options presence: true do
     validates :name
     validates :description
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
-    validates :category_id, :condition_id, :shipping_fee_status_id, :shipping_area_id, :shipping_days_id, numericality: { other_than: 1, message: "を選択してください" }
+    validates :category_id, :condition_id, :shipping_fee_id, :shipping_area_id, :shipping_day_id, numericality: { other_than: 1, message: "を選択してください" }
   end
 
 end
