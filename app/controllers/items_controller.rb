637bd0e9b8d1665fc4a23fef20e22ec1a1b0924b
takehiro-yamamoto <xj543ade@gmail.com>
2025-05-16 17:33:43 +0900

    ストロングパラメーターを記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,4 +17,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_status_id, :shipping_area_id, :shipping_days_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
