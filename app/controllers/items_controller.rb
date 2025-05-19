bbb5e7166c92d7bd2e6eff5a6b8efa6f9521d453
takehiro-yamamoto <xj543ade@gmail.com>
2025-05-19 15:38:44 +0900

    商品一覧機能設定（購入機能の一部も下地として設定）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @items = Item.includes(:user).order(created_at: :desc)
   end
 
   def new
@@ -15,6 +16,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+  @item = Item.find(params[:id])
   end
 
   private
