5b4e3d20d0bd85059f54a116568374be35a9e20c
takehiro-yamamoto <xj543ade@gmail.com>
2025-05-19 14:06:43 +0900

    itemモデル単体テストのcontext追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品出品機能' do
-    it '全ての情報が正しく入力されていれば出品できる' do
+    context '出品できる場合' do
+     it '全ての情報が正しく入力されていれば出品できる' do
       expect(@item).to be_valid
+     end
     end
 
     context '出品できない場合' do
