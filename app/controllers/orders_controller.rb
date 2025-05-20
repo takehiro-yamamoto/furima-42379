9f2b472ffa4e4805faab16ecbd8b679693f5f8b6
takehiro-yamamoto <xj543ade@gmail.com>
2025-05-20 21:20:31 +0900

    ordersコントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,45 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :redirect_if_invalid
+
+  def index
+    @item = Item.find(params[:item_id])
+    @order_shipping_address = OrderShippingAddress.new
+  end
+
+  def create
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if @order_shipping_address.valid?
+      pay_item  # クレカ決済処理
+      @order_shipping_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+          .merge(user_id: current_user.id, item_id: @item.id, token: params[:order_shipping_address][:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_if_invalid
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
