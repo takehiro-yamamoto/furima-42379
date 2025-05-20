3c71b417e84a651c4fbe5d11a7f609280a388361
takehiro-yamamoto <xj543ade@gmail.com>
2025-05-20 15:32:52 +0900

    ログイン時のみ削除可能にする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,8 +34,18 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+def destroy
+  item = Item.find(params[:id])
+  if item.user == current_user
+    item.destroy
+    redirect_to root_path
+  else
+    redirect_to root_path
+  end
 end
 
+end
 private
 
 def item_params
